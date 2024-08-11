f095ecddadd342400e078cb9c2fe52bdd2421129
Gerzsenyi Patrik <patrikchannelmc@gmail.com>
2024-08-11 21:34:00 +0200

    Created token entity and repository

---
+++ book-sweeper/src/main/java/com/cicau/book/entity/Token.java
@@ -0,0 +1,32 @@
+package com.cicau.book.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "token")
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
