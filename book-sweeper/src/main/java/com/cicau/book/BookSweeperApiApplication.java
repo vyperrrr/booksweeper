b3b10725fd8d265033b8ef852af455d24682b3c8
Gerzsenyi Patrik <patrikchannelmc@gmail.com>
2024-08-11 18:29:03 +0200

    Project setup

---
+++ book-sweeper/src/main/java/com/cicau/book/BookSweeperApiApplication.java
@@ -0,0 +1,13 @@
+package com.cicau.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookSweeperApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookSweeperApiApplication.class, args);
+	}
+
+}
