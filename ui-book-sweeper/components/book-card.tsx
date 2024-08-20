113ea994e63b419119c2d581757c717b096af005
Gerzsenyi Patrik <patrikchannelmc@gmail.com>
2024-08-20 18:26:28 +0200

    Added function button to book card

--- ui-book-sweeper/components/book-card.tsx
+++ ui-book-sweeper/components/book-card.tsx
@@ -1,6 +1,7 @@
 import {Card} from "@/components/ui/card";
 import {StarIcon} from "lucide-react";
 import Link from "next/link";
+import {Button} from "@/components/ui/button";
 
 export const BookCard = () => {
     return (
@@ -24,7 +25,7 @@ export const BookCard = () => {
                     </div>
                 </div>
                 <div
-                    className="absolute space-y-2 bottom-0 left-0 right-0 px-4 pb-4 pt-4 hidden group-hover:block bg-secondary/80 h-full overflow-y-auto no-scrollbar max-h-full">
+                    className="absolute space-y-2 bottom-0 left-0 right-0 px-4 py-4 hidden group-hover:block bg-secondary/80 h-full overflow-y-auto no-scrollbar max-h-full">
                     <div>
                         <h3 className="text-lg font-bold text-white">The Great Gatsby</h3>
                         <div className="flex items-center gap-2 text-sm text-white">
@@ -34,8 +35,18 @@ export const BookCard = () => {
                         </div>
                     </div>
                     <div className="text-muted-foreground text-sm text-pretty indent-8 text-justify">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus, nisl sit amet pulvinar pellentesque, orci turpis auctor dui, eget consectetur erat quam at magna. Nam tempus imperdiet sagittis. Nullam semper consequat malesuada. Integer bibendum condimentum lobortis. Quisque in elementum augue. Nulla suscipit lectus at turpis luctus, volutpat rutrum quam aliquet. Maecenas aliquam ipsum ac convallis ultrices. Proin id velit ac purus convallis malesuada sed id quam. Quisque facilisis mi libero, at condimentum lacus aliquam vitae. Pellentesque tortor ipsum, congue vel maximus nec, volutpat laoreet dui. Mauris in mauris ut felis tristique suscipit vehicula eu neque.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam finibus, nisl sit amet pulvinar
+                        pellentesque, orci turpis auctor dui, eget consectetur erat quam at magna. Nam tempus imperdiet
+                        sagittis. Nullam semper consequat malesuada. Integer bibendum condimentum lobortis. Quisque in
+                        elementum augue. Nulla suscipit lectus at turpis luctus, volutpat rutrum quam aliquet. Maecenas
+                        aliquam ipsum ac convallis ultrices. Proin id velit ac purus convallis malesuada sed id quam.
+                        Quisque facilisis mi libero, at condimentum lacus aliquam vitae. Pellentesque tortor ipsum,
+                        congue vel maximus nec, volutpat laoreet dui. Mauris in mauris ut felis tristique suscipit
+                        vehicula eu neque.
                     </div>
+                </div>
+                <div className="absolute bottom-0 left-0 right-0 hidden group-hover:block bg-secondary/40 backdrop-blur py-4 px-4">
+                   <Button size="lg" className="w-full text-mute font-bold">Borrow book</Button>
                 </div>
             </Link>
         </Card>
